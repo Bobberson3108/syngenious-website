eaf8963e4ff32cb4552ed25191e6c2bf9c4aff3d
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-04-13 21:24:51 +0100

    competitoin entry?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,17 +12,16 @@
 
 <div class="overflow-hidden">
     <div class="container px-4 sm:px-6 md:px-8 lg:pl-12 py-8">
-        <!-- Added responsive padding and margin classes -->
         <div class="mt-5 sm:mt-10 mb-20 sm:mb-40">
             <div class="col-span-2 p-5 w-full z-0">
-                <div class="z-0" data-aos="fade-up">
-                    <h1 class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold font-syne dark:text-white">
+                <div class="z-0">
+                    <h1 class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-semibold font-syne dark:text-white" data-aos="fade-up">
                         <span class="block">Creators need a</span>
                         <span class="block">collaborative, social</span>
                         <span class="block">and innovative</span>
                         <span class="block">digital space.</span>
                     </h1>
-                    <h2 class="text-2xl sm:text-3xl md:text-4xl xl:text-5xl lg:text-6xl mt-3 font-quattrocento text-primary-700 dark:text-secondary-500">Come build one with us.</h2>
+                    <h2 class="text-2xl sm:text-3xl md:text-4xl xl:text-5xl lg:text-7xl mt-3 font-syne font-semibold text-primary-700 dark:text-secondary-500" data-aos="fade-up" data-aos-delay=300>Come build one with us.</h2>
                 </div>
             </div>
         </div>
@@ -30,23 +29,22 @@
     <div class="w-screen py-16 sm:py-24 md:py-32 pl-4 sm:pl-6 md:pl-8 lg:pl-12 bg-primary-800 dark:bg-transparent dark:bg-gradient-to-r dark:from-primary-900 dark:to-primary-800 -skew-y-12">
         <div class="flex-row flex justify-between">
             <div class="ml-0 sm:ml-10 text-center flex items-center justify-center">
-                <div data-aos="fade-up">
-                    <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold font-syne text-secondary-400">Get your projects out there</h1>
-                    <a href="https://discord.gg/Cz95wV44nA"><h2 class="text-3xl sm:text-4xl md:text-5xl font-quattrocento underline  text-white hover:text-primary-100 dark:text-primary-100 dark:hover:text-primary-200">In our discord.</h2></a>
+                <div>
+                    <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold font-syne text-secondary-400" data-aos="fade-up">Get your projects out there</h1>
+                    <a href="https://discord.gg/Cz95wV44nA"><h2 class="text-3xl sm:text-4xl md:text-5xl underline  text-white hover:text-primary-100 dark:text-primary-100 dark:hover:text-primary-200 font-syne" data-aos="fade-up" data-aos-delay=300>In our discord.</h2></a>
                 </div>
             </div>
-            
             <div data-aos="fade-up">
                 <img class="z-10 skew-y-12 mr-0 sm:mr-10 md:mr-20" src="img\external-brand-marketing-strategy-wanicon-lineal-wanicon.png">
             </div>
         </div>
     </div>
     <div class="w-screen py-8 pl-4 sm:pl-6 md:pl-8 lg:pl-12">
-        <div class="mt-24 sm:mt-48 flex flex-col sm:flex-row justify-between" data-aos="fade-up">
-            <img class="w-48 sm:w-64 md:w-72 lg:w-auto max-w-full mb-8 sm:mb-0 dark:invert" src="img\external-trophy-award-and-success-wanicon-lineal-wanicon.png">
+        <div class="mt-24 sm:mt-48 flex flex-col sm:flex-row justify-between">
+            <img class="w-48 sm:w-64 md:w-72 lg:w-auto max-w-full mb-8 sm:mb-0 dark:invert" src="img\external-trophy-award-and-success-wanicon-lineal-wanicon.png" data-aos="fade-up" data-aos-delay=150>
             <div class="text-center justify-center w-full" data-aos="fade-up">
                 <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold font-syne dark:text-white">Make your<br>mark in our<br> competitions.</h1>
-                <h2 class="text-3xl sm:text-4xl md:text-5xl font-quattrocento underline text-primary-700 hover:text-primary-500 dark:text-secondary-500 dark:hover:text-secondary-600">
+                <h2 class="text-3xl sm:text-4xl md:text-5xl font-syne underline text-primary-700 hover:text-primary-500 dark:text-secondary-500 dark:hover:text-secondary-600" data-aos="fade-up" data-aos-delay=300>
                     <a href="/competitions">See what's on right now</a>
                 </h2>
             </div>
--- src/routes/competitions/+page.svelte
+++ src/routes/competitions/+page.svelte
@@ -1,13 +1,24 @@
-<h1 class="font-syne font-bold w-full text-center text-3xl sm:text-4xl md:text-5xl mb-10 mt-3 text-primary-500 dark:text-white">Competitions</h1>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import AOS from 'aos';
+    import 'aos/dist/aos.css';
+
+    onMount(() => {
+        AOS.init();
+        AOS.refresh();
+    });
+</script>
+
+<h1 class="font-syne font-bold w-full text-center text-3xl sm:text-4xl md:text-5xl mb-10 mt-3 text-primary-500 dark:text-white" data-aos="fade-up">Competitions</h1>
 
 <div class="px-10 py-5">
-    <h2 class="font-syne font-bold w-full text-2xl sm:text-3xl md:text-4xl mb-10 mt-3 text-primary-400 dark:text-white">Current Competitions</h2>
-    <div class="flex flex-col sm:flex-row w-full dark:text-grey-300 rounded-sm p-3 border-[3px]">
+    <h2 class="font-syne font-bold w-full text-2xl sm:text-3xl md:text-4xl mb-10 mt-3 text-primary-400 dark:text-white" data-aos="fade-up" data-aos-delay=50>Current Competitions</h2>
+    <div class="flex flex-col sm:flex-row w-full dark:text-grey-300 rounded-sm p-3 border-[3px]" data-aos="fade-up" data-aos-delay=100>
         <div class="w-full sm:w-1/2 h-full mb-4 sm:mb-0 sm:flex sm:justify-center">
             <img src="img\external-magic-fairytale-wanicon-lineal-wanicon.png" class="w-2/3 sm:w-full mx-auto sm:mx-0 max-w-lg dark:invert"/>
         </div>
         <div class="flex flex-col w-full sm:w-1/2 h-full px-4 sm:px-0">
-            <h2 class="font-syne text-center h-full font-semibold text-2xl sm:text-3xl md:text-4xl mb-4 ">DREAMS</h2>
+            <h2 class="font-syne text-center h-full font-semibold text-2xl sm:text-3xl md:text-4xl mb-4">DREAMS</h2>
 
             <div class="p-4">
                 <p class="mb-2">
@@ -25,14 +36,24 @@
 
                 <p class="font-bold text-primary-600 dark:text-primary-400">Entries to be submitted by Friday 14th April. <br>Winners announched on Friday 21st April.</p>
 
-                <div class="w-full flex justify-center my-4">
-                    <a href="/competitions/enter" class="text-center text-primary-100 text-2xl font-syne p-2
-                    bg-primary-700 hover:bg-primary-800 dark:bg-primary-800 dark:hover:bg-primary-900">Enter now</a>
+                <div class="w-full flex justify-center my-4 space-x-4">
+                    <a
+                        href="/competitions/enter?competition=art"
+                        class="text-center text-primary-100 text-2xl font-syne p-2 bg-primary-700 hover:bg-primary-800 dark:bg-primary-800 dark:hover:bg-primary-900"
+                    >
+                        Enter Art Competition
+                    </a>
+                    <a
+                        href="/competitions/enter?competition=music"
+                        class="text-center text-primary-100 text-2xl font-syne p-2 bg-primary-700 hover:bg-primary-800 dark:bg-primary-800 dark:hover:bg-primary-900"
+                    >
+                        Enter Music Competition
+                    </a>
                 </div>
             </div>
         </div>
     </div>
-    <div class="dark:text-grey-300 p-3 border-[3px] border-t-0">
+    <div class="dark:text-grey-300 p-3 border-[3px] border-t-0" data-aos="fade-up" data-aos-delay=150>
         <h2 class="font-syne text-center font-semibold text-2xl sm:text-3xl md:text-4xl mb-4">CODING COMPETITION</h2>
         <div class="flex flex-col space-y-3 sm:flex-row sm:space-x-3 font-semibold text-lg">
             <div>
