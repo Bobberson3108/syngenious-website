001c2b0b778bdfc7a36550baeee5344494d3f1ec
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-04-13 21:28:57 +0100

    something

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,2 +1,10 @@
-import { redirect } from "@sveltejs/kit"
-import type { PageLoad } from "./$types"
+import { redirect } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+
+export const load = (( { parent } ) => {
+    const { session } = parent;
+
+    if (session.user) {
+        throw redirect('/')
+    }
+})
