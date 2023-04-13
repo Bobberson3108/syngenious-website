ba35ed35b5f035609429ee35f58cc3674b4e1b26
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-04-13 17:31:48 +0100

    hide about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
     import { onMount } from 'svelte';
     import AOS from 'aos';
     import 'aos/dist/aos.css';
@@ -93,4 +93,4 @@
         </div>
     </div>
 </div>
-
+ -->
