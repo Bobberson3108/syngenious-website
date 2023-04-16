1fffd425e02680210da097ec1cf2220e3f7db2bd
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-04-16 20:13:18 +0100

    fix text colour

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,21 +11,21 @@
     import InteractiveName from '$lib/components/InteractiveName.svelte';
 </script>
 
-<h1 class="font-syne font-bold w-full text-center text-3xl sm:text-4xl md:text-5xl mt-3 mb-16 text-primary-500 dark:text-white" data-aos="fade-up">About</h1>
+<h1 class="font-syne font-bold w-full text-center text-3xl sm:text-4xl md:text-5xl mt-3 mb-16 text-primary-700 dark:text-white" data-aos="fade-up">About</h1>
 
 <h2 data-aos="fade-up">What is Syngenious?</h2>
 
-<p class="text-lg sm:text-xl md:text-2xl text-gray-700 dark:text-white" data-aos="fade-up">At its core, Syngenious a digital space where young people can come together to explore their passions, create cool stuff and make a positive impact on the world.</p>
+<p class="text-lg sm:text-xl md:text-2xl text-gray-700 text-primary-700 dark:text-white" data-aos="fade-up">At its core, Syngenious a digital space where young people can come together to explore their passions, create cool stuff and make a positive impact on the world.</p>
 
 <h2>The Name</h2>
 <InteractiveName />
 
 <style lang="postcss">
     h2 {
-        @apply font-syne font-bold text-2xl sm:text-3xl md:text-4xl mb-8 ml-4 text-primary-500 dark:text-white;
+        @apply font-syne font-bold text-2xl sm:text-3xl md:text-4xl mb-8 ml-4 text-primary-700 dark:text-white;
     }
 
     p {
-        @apply font-syne mb-8 ml-4 text-primary-500 dark:text-white;
+        @apply font-syne mb-8 ml-4 text-primary-700 dark:text-white;
     }
 </style>
