b04675c96d797b52aba6ae758e56fd6a09897e33
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-05-10 17:32:40 +0100

    make sun yellow

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -67,8 +67,8 @@
           </li>
         {/if}
     </ul>
-    <button class="px-3 py-2 mr-5" on:click=   {toggleTheme}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-grey-600 dark:stroke-grey-400 hover:stroke-grey-700 dark:hover:stroke-grey-500">
+    <button class="px-3 my-2 mr-5" on:click=   {toggleTheme}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-grey-600 dark:stroke-secondary-600 hover:stroke-grey-700 dark:hover:stroke-secondary-500 clickable">
             {#if $theme === 'light'}
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
             {:else}
