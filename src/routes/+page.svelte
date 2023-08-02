fdba02ff445590b25e9e6c7e0a410b0aa1386c25
msdq17 <mabdullahsadiq299@gmail.com>
2023-08-03 00:18:54 +0100

    Just working with logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<body>
+    <nav class="navbar">
+        <div class="logoNav"></div>
+        <div class="navButtonsContainer">
+            <button class="joinUsNav">Join us</button>
+            <button class="signInNav">Sign In</button>
+        </div>
+    </nav>
+</body>
+
+<style>
+    body{
+        min-height: 300vh;
+        background-color: #01131D;
+        padding: 0;
+        margin: 0;
+    }
+
+    .navbar{
+        position: fixed;
+        top: 0%;
+        width: 100%;
+        height: 75px;
+        border: 1px solid #fff;
+        background: rgba(1, 19, 29, 0.89);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .logoNav{
+        background-image: url('/logoDarkMode.png');
+        height: 50px;
+        width: 100px;
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+</style>
