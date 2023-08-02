e3f3492895c24d3c944a475de77d2ac6b6d9b3b5
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-08-02 20:50:10 +0100

    replace everything

--- src/routes/projects/+page.svelte
+++
@@ -1,8 +0,0 @@
-<svelte:head>
-    <title>Projects | Syngenious</title>
-</svelte:head>
-
-
-<div class="flex items-center justify-center">
-    <h1 class="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold font-syne dark:text-white">Coming soon...</h1>
-</div> 
