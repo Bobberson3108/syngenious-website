2de978b1df87edf7048663321997f19faef08194
msdq17 <mabdullahsadiq299@gmail.com>
2023-08-09 18:34:41 +0100

    sandjsandinsa;i jdsa

--- src/lib/components/SlidingButton.svelte
+++ src/lib/components/SlidingButton.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="introCtaButton overflow-hidden relative uppercase w-[auto] px-[15px] h-[auto] py-[10px] border-2 border-dark dark:border-light flex justify-center items-center rounded-[10px] mt-[35px] text-dark dark:text-light hover:text-light dark:hover:text-dark">
-    <h3 class="z-2 font-display text-[{textSize}px]">{buttonText}</h3>
+    <h3 id="buttonText" class="buttonText z-2 font-display">{buttonText}</h3>
     <div class="introButtonHover z-[-1] ease-out duration-[200ms] absolute w-full h-full bg-dark dark:bg-light top-[100%]"></div>
 </div>
 
