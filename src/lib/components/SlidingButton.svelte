654cf355a298fc8d2eb64343f52a15c4678e1590
msdq17 <mabdullahsadiq299@gmail.com>
2023-08-09 20:37:28 +0100

    fixed the button font size error using onMount

--- src/lib/components/SlidingButton.svelte
+++ src/lib/components/SlidingButton.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
-    export let buttonText:string;
-    export let textSize:number;
+    export let buttonText: string;
+    export let textSize: number;
+    let buttonTextElement: HTMLDivElement;
+
+    // Use onMount to access and modify the element's style after it's bound
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        // Make sure buttonTextElement is defined before accessing its style property
+        if (buttonTextElement) {
+            buttonTextElement.style.fontSize = textSize + "px";
+        }
+    });
 </script>
 
 <div class="introCtaButton overflow-hidden relative uppercase w-[auto] px-[15px] h-[auto] py-[10px] border-2 border-dark dark:border-light flex justify-center items-center rounded-[10px] mt-[35px] text-dark dark:text-light hover:text-light dark:hover:text-dark">
-    <h3 id="buttonText" class="buttonText z-2 font-display">{buttonText}</h3>
+    <div bind:this={buttonTextElement} id="buttonText" class="buttonText z-2 font-display font-medium">{buttonText}</div>
     <div class="introButtonHover z-[-1] ease-out duration-[200ms] absolute w-full h-full bg-dark dark:bg-light top-[100%]"></div>
 </div>
 
