01ee15e7a85cff83a7574a0557155aef55c86e47
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-08-20 11:38:33 +0100

    checkpoint

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,24 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
-import Discord from '@auth/core/providers/discord';
-import CredentialsProvider from '@auth/core/providers/credentials';
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import { clientPromise } from '$lib/db';
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import Discord from '@auth/core/providers/discord';
+import EmailProvider from '@auth/core/providers/email';
 
-import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } from '$env/static/private';
+import { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, GITHUB_ID, GITHUB_SECRET, SPOTIFY_ID, SPOTIFY_SECRET, EMAIL_SERVER, EMAIL_FROM} from '$env/static/private';
 
 export const handle = SvelteKitAuth({
     providers: [
+        EmailProvider({
+            server: EMAIL_SERVER,
+            from: EMAIL_FROM,
+        }),
         Discord({
             clientId: DISCORD_CLIENT_ID,
             clientSecret: DISCORD_CLIENT_SECRET,
         }),
     ],
-    adapter: MongoDBAdapter(clientPromise),
+    adapter: MongoDBAdapter(clientPromise, {
+        databaseName: 'syngenious'}),
     callbacks: {
         async session( { session, token, user} ) {
             return { ...session, user};
