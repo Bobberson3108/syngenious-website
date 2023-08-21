a00deee063228c13f0d268364d76062f1e981e84
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-08-21 17:55:59 +0100

    user_auth kinda working

--- src/lib/components/layout/NavBar.svelte
+++ src/lib/components/layout/NavBar.svelte
@@ -22,7 +22,7 @@
     <div id="logo" class="logo cursor-pointer h-[50px] w-[200px] bg-logoLightMode dark:bg-logoDarkMode bg-contain bg-center bg-no-repeat"></div>
     <div class="flex justify-center items-center">
         {#if Object.keys($page.data.session || {}).length > 0}
-            <a id="dashboardLink" class="menuButton h-9 w-auto bg-dark hover:bg-light px-4 font-display font-medium text-light hover:text-dark ease-out duration-200 uppercase rounded-xl border-2 border-dark">Forum</a>
+            <a href="/forum" id="dashboardLink" class="menuButton h-9 w-auto bg-dark hover:bg-light px-4 font-display font-medium text-light hover:text-dark ease-out duration-200 uppercase rounded-xl border-2 border-dark">Forum</a>
             <a id="dashboardLink" class="menuButton h-9 w-auto bg-dark hover:bg-light px-4 font-display font-medium text-light hover:text-dark ease-out duration-200 uppercase rounded-xl border-2 border-dark">Dashboard</a>
              {#if $page.data?.session?.user?.image}
                 <div class="relative" on:blur={closeProfileMenu} >
---
+++ src/routes/forum/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+   
+
+    async function test() {
+        await fetch('http://localhost:5173/api/forum/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "title": "hi",
+                "content": "hi",
+                tags: ["philosophy", "cs", "competition"]
+            }),
+        });
+    };
+</script>
+
+
+<div class="h-full flex-col items-center align-middle">
+    <button class="text-light text-[200px] pt-[10vh]" on:click={test}>FJDSFDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDFDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD</button>
+</div>
